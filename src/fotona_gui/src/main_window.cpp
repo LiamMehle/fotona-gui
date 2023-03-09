0081c823f52f26f49cd722c4584f6bc9c56aae8a
Liam Mehle <liammehlex@gmail.com>
2023-03-09 13:54:22 +0100

    moved launch file to correct spot

--- src/fotona_gui/src/main_window.cpp
+++ src/fotona_gui/src/main_window.cpp
@@ -83,7 +83,7 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
 	rviz::Display& pointcloud = *this->manager->createDisplay("rviz/PointCloud2", "pico flexx pointcloud", true);
 	pointcloud.subProp("Alpha")->setValue(0.5f);
 	pointcloud.subProp("Channel")->setValue("Intensity");
-	pointcloud.subProp("Size (m)")->setValue(0.002);
+	pointcloud.subProp("Size (m)")->setValue(0.003);
 	pointcloud.subProp("Color Transformer")->setValue("AxisColor");
 	pointcloud.subProp("Axis")->setValue("Z");
 	// Position Transformer
