5698f8ebfa20631a42c0c0e30aace885254cd514
Liam Mehle <liammehlex@gmail.com>
2023-03-16 15:36:14 +0100

    WORKING SELECT TOOL

--- src/fotona_gui/src/main_window_constructor.cpp
+++ src/fotona_gui/src/main_window_constructor.cpp
@@ -1,9 +1,12 @@
 #include <exception>
 #include "main_window.hpp"
-#include "rviz/view_manager.h"
-#include "rviz/visualization_frame.h"
-#include "rviz/tool_manager.h"
+#include <rviz/view_manager.h>
+#include <rviz/visualization_frame.h>
+#include <rviz/tool_manager.h>
 #include <qstringlist.h>
+
+#include <rviz/default_plugin/tools/selection_tool.h>
+#include <rviz/selection/selection_manager.h>
 
 namespace RvizDisplayType {
 	auto const Grid   = "rviz/Grid";
@@ -26,8 +29,9 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
 	auto central_widget  = new QWidget(parent);
 	auto visualization_frame = new rviz::VisualizationFrame(parent);
 	auto render_panel = new rviz::RenderPanel(parent);
+	render_panel->setMouseTracking(true);
 	auto visualization_manager = new rviz::VisualizationManager(render_panel, visualization_frame);
-
+	render_panel->initialize(visualization_manager->getSceneManager(), visualization_manager);
 	// Pointers can be indexed into and restrict is an non-standard. A reference fixes it.
 
 	central_widget->setLayout(main_layout);
@@ -87,6 +91,19 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
 	this->pointcloud->subProp("Color Transformer")->setValue("AxisColor");
 	this->pointcloud->subProp("Axis")->setValue("Z");
 	this->pointcloud->setTopic("/pico_flexx/points", "sensor_msgs/PointCloud2");
+	auto tool_manager = visualization_manager->getToolManager();
+	rviz::SelectionTool* pointcloud_select_tool = static_cast<rviz::SelectionTool*>(tool_manager->addTool(pointcloud_select_tool_name));
+	if (pointcloud_select_tool == nullptr)
+		throw std::runtime_error("pointcloud_select_tool is not of type rviz::SelectionTool");
+	tool_manager->setCurrentTool(pointcloud_select_tool);
+	pointcloud_select_tool->activate();
+	pointcloud_select_tool->initialize(visualization_manager);  // missing piece
+	tool_manager->setParent(parent);
+	puts("---------------------------------");
+	printf("ClassId:     %s\n", tool_manager->getCurrentTool()->getClassId().toLocal8Bit().data());
+	printf("Name:        %s\n", tool_manager->getCurrentTool()->getName().toLocal8Bit().data());
+	printf("Description: %s\n", tool_manager->getCurrentTool()->getDescription().toLocal8Bit().data());
+	puts("---------------------------------");
 	// auto& tool_manager = *this->manager->getToolManager();
 	// auto tool_classes = tool_manager.getToolClasses();
 	// auto const tool_classes_count = tool_classes.size();
@@ -98,6 +115,9 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
 	// // setting up a valid view transform such that the camera won't complain
 	// auto view_matrix = calculate_projection_matrix(-1, 1, -1, 1, 0.001, 1000);
 	// camera->setCustomViewMatrix(true, view_matrix);  // this will be updated in `set_view_matrix`
+
+	auto selection_manager = visualization_manager->getSelectionManager();
+	selection_manager->enableInteraction(true);
 
 	visualization_manager->setFixedFrame(rviz_fixed_frame);
 
